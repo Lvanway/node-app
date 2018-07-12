96ad0448c68af7d34e4d10c831d92788eb61b99c
MarkovBrain <34607460+MarkovBrain@users.noreply.github.com>
2018-07-11 22:56:15 -0400

    Fixed script pathing

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,6 +2,10 @@
 
 eventsApp.controller('EventController',
   function EventController($scope) {
-
+      $scope.event = {
+          name: 'Angular Bootcamp',
+          date: '1/1/2019',
+          time: '10:30 AM'
+      }
   }
 );
